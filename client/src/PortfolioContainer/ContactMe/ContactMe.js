2812a0e94bfa1cba4d3b1a3dba6f426c33dbfb87
rahulrajamani31 <rahulrajamani310702@gmail.com>
2023-03-07 17:46:11 +0530

    first commit

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -17,13 +17,11 @@ export default function Contactme(props) {
   };
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [banner, setBanner] = useState("");
   const [bool, setBool] = useState(false);
-
   const handleName = (e) => {
     setName(e.target.value);
   };
@@ -35,33 +33,31 @@ export default function Contactme(props) {
   };
   console.log(name);
   const submitForm = async (e) => {
-    e.preventDefault();
-    try {
-      let data = {
-        name,
-        email,
-        message,
-      };
-      setBool(true);
-      const res = await axios.post("/contact", data);
-      if (name.length === 0 || email.length === 0 || message.length === 0) {
-        setBanner(res.data.msg);
-        toast.error(res.data.msg);
-        setBool(false);
-      } else if (res.status === 200) {
-        setBanner(res.data.msg);
-        toast.success(res.data.msg);
-        setBool(false);
-
-        setName("");
-        setEmail("");
-        setMessage("");
-      }
-    } catch (error) {
-      console.log(error);
+    // e.preventDefault();
+    // try {
+    //   let data = {
+    //     name,
+    //     email,
+    //     message,
+    //   };
+    //   setBool(true);
+    // const res = await axios.post("/contact", data);
+    if (name.length === 0 || email.length === 0 || message.length === 0) {
+      setBanner("res.data.msg");
+      toast.error("res.data.msg");
+      setBool(false);
+    } else {
+      setBanner("Message Sent successfully");
+      toast.success("Success");
+      setBool(false);
+      setName("");
+      setEmail("");
+      setMessage("");
     }
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
-
   return (
     <div className="main-container fade-in" id={props.id || ""}>
       <ScreenHeading subHeading={"Lets keep in Touch"} title={"Contact Me"} />
@@ -93,24 +89,21 @@ export default function Contactme(props) {
             <p>{banner}</p>
             <label htmlFor="name">Name</label>
             <input type="text" onChange={handleName} value={name} />
-
             <label htmlFor="email">Email</label>
             <input type="email" onChange={handleEmail} value={email} />
-
             <label htmlFor="message">Message</label>
             <textarea type="text" onChange={handleMessage} value={message} />
-
             <div className="send-btn">
-              <button type="submit">
+              <button type="submit" onchange={submitForm}>
                 send
                 <i className="fa fa-paper-plane" />
-                {bool ? (
+                {/* {bool ? (
                   <b className="load">
                     <img src={load1} alt="image not responding" />
                   </b>
                 ) : (
                   ""
-                )}
+                )} */}
               </button>
             </div>
           </form>
